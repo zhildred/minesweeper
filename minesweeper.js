f0adc95319275bd547591626402425c761b90719
Zachary <zachildred@gmail.com>
2020-03-05 11:21:14 +1300

    Add ability for cells to display the number of surrounding mines

--- minesweeper.js
+++ minesweeper.js
@@ -14,6 +14,10 @@ var board = {
 }
 
 function startGame () {
+  for(var i = 0; i < board.cells.length; i++) {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
+    console.log(board.cells[i].surroundingMines)
+  }
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
@@ -29,14 +33,17 @@ function checkForWin () {
   //   lib.displayMessage('You win!')
 }
 
-// Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`: 
-//
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-//
-// It will return cell objects in an array. You should loop through 
-// them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
+  var surroundingCells = getSurroundingCells(cell.row, cell.col)
+
+  var mineCount = 0
+
+  for (var i = 0; i < surroundingCells.length; i++) {
+    if(surroundingCells[i].isMine) {
+      mineCount++
+    }
+  }
+
+  return mineCount
 }
 
