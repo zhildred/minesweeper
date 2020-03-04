dc9a9b8cb5e8cb1fe114d2332dda87ed03dd2158
Zachary <zachildred@gmail.com>
2020-03-05 11:43:51 +1300

    Add function to check if player has won game

--- minesweeper.js
+++ minesweeper.js
@@ -14,23 +14,29 @@ var board = {
 }
 
 function startGame () {
-  for(var i = 0; i < board.cells.length; i++) {
+  document.addEventListener('click', checkForWin)
+  document.addEventListener('contextmenu', checkForWin)
+
+  for (var i = 0; i < board.cells.length; i++) {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
-    console.log(board.cells[i].surroundingMines)
   }
+  
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
 
-// Define this function to look for a win condition:
-//
-// 1. Are all of the cells that are NOT mines visible?
-// 2. Are all of the mines marked?
 function checkForWin () {
+  for (var i = 0; i < board.cells.length; i++) {
+    if (board.cells[i].isMine && !board.cells[i].isMarked) {
+      return
+    }
 
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+    if (!board.cells[i].isMine && board.cells[i].hidden) {
+      return
+    }
+  }
+
+  lib.displayMessage('You win!')
 }
 
 function countSurroundingMines (cell) {
@@ -39,7 +45,7 @@ function countSurroundingMines (cell) {
   var mineCount = 0
 
   for (var i = 0; i < surroundingCells.length; i++) {
-    if(surroundingCells[i].isMine) {
+    if (surroundingCells[i].isMine) {
       mineCount++
     }
   }
